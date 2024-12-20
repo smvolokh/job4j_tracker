ebeef2c4b1ee92a82c15598fd9c6ecda4b492adb
smvolokh <sema.volokh@yandex.ru>
2024-12-20 17:22:18 +0300

    Add support for 3d in Point class, add test for that logic

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -1,9 +1,10 @@
 package ru.job4j.oop;
 
 
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import static org.assertj.core.api.Assertions.assertThat;
 public class PointTest {
 
     @Test
@@ -18,6 +19,13 @@ public class PointTest {
         Point first = new Point(5, 5);
         Point second = new Point(5, 9);
         Assertions.assertEquals(first.distance(second), 4);
+    }
+
+    @Test
+    public void when345And567Then346() {
+        Point first = new Point(3, 4, 5);
+        Point second = new Point(5, 6, 7);
+        assertThat(first.distance3d(second)).isCloseTo(3.46D, Offset.offset(0.01));
     }
 
 }
