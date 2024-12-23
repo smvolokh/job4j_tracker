070d93a4a825f5e3cdce50a064080a176803bb9f
smvolokh <sema.volokh@yandex.ru>
2024-12-23 15:45:31 +0300

    Add different pizzas

---
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class PizzaExtraCheese extends Pizza {
+
+    @Override
+    public String name() {
+        return super.name() + " + extra cheese";
+    }
+
+}
---
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class PizzaExtraCheeseExtraTomato extends Pizza {
+
+    @Override
+    public String name() {
+        return super.name() + " + extra cheese + extra tomato";
+    }
+}
