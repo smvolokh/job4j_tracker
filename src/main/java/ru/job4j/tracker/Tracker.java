7b82381f2508b52fe78cb0b2d7d2439214e4300f
smvolokh <sema.volokh@yandex.ru>
2024-12-24 16:38:20 +0300

    Add tracker methods and tests

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,27 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] copy = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            copy[i] = items[i];
+        }
+        return copy;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsByName = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null && key.equals(item.getName())) {
+                itemsByName[size] = items[i];
+                size++;
+            }
+        }
+        itemsByName = Arrays.copyOf(itemsByName, size);
+        return itemsByName;
     }
 }
