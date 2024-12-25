17ff11aebb1f251e698aaf81ff3e089628bbc526
smvolokh <sema.volokh@yandex.ru>
2024-12-25 19:15:58 +0300

    Refactor delete method

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -53,9 +53,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean result = index != -1;
         if (result) {
-            int start = index + 1;
-            int length = size - index - 1;
-            System.arraycopy(items, start, items, index, length);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
