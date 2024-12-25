43f5aaf9007b80b2c55500c3b9018cd2857b2e01
smvolokh <sema.volokh@yandex.ru>
2024-12-25 18:22:44 +0300

    Fix for

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -2,10 +2,8 @@ package ru.job4j.pojo;
 
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
-        for (int i = index + 1; i < products.length; i++) {
-            Product temp = products[i - 1];
-            products[i - 1] = products[i];
-            products[i] = temp;
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
         }
         products[products.length - 1] = null;
         return products;
