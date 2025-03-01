c7ed33440bac832455b4666019175b88e3908074
smvolokh <sema.volokh@yandex.ru>
2025-03-01 18:23:39 +0300

    Add SingleTracker class

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,32 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+    private final Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+}
