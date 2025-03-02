01ad335a4e365d66db2bda65e464387aed489afb
smvolokh <sema.volokh@yandex.ru>
2025-03-02 11:59:25 +0300

    Add static field and method for getting it

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,9 +1,17 @@
 package ru.job4j.tracker;
 
 public final class SingleTracker {
+    private static SingleTracker instance = null;
     private final Tracker tracker = new Tracker();
 
     private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
     }
 
     public Item add(Item item) {
