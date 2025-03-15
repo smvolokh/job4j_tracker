46db2b0b583b72b183eb74a71e9557af674408bc
svolokh <vtb4082582@region.vtb.ru>
2025-03-15 15:50:13 +0300

    Add UsageArrayList and example

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
+        names.forEach(System.out::println);
+    }
+}
