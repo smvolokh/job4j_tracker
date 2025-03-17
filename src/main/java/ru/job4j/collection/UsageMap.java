f275654f89a32839482ef28b149b3248e76d05bd
svolokh <vtb4082582@region.vtb.ru>
2025-03-17 14:28:50 +0300

    Add UsageMap class

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("smvolokh@qwoke.ru", "Volokh Semen M");
+        for (String key : map.keySet()) {
+            System.out.println("Key: " + key + " Value: " + map.get(key));
+        }
+    }
+}
