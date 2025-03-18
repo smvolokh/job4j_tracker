923bf5d2fcd3007bb2803d0a2745c6b0131de3b1
svolokh <vtb4082582@region.vtb.ru>
2025-03-18 10:27:29 +0300

    Add some elements to the map

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
         map.put("smvolokh@qwoke.ru", "Volokh Semen M");
+        map.put("key", "value");
+        map.put("key", "anotherValue");
+        map.put("school", "1000");
         for (String key : map.keySet()) {
             System.out.println("Key: " + key + " Value: " + map.get(key));
         }
